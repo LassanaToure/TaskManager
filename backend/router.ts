d2e35b6a86f8875e2da70f9e9f7f056a41039522
Toure Lassana <l.toure910@laposte.net>
2024-11-05 23:08:35 +0100

    commit

--- backend/router.ts
+++ backend/router.ts
@@ -2,15 +2,22 @@ import { IncomingMessage, ServerResponse } from 'http';
 import getTasks from './routes/getTasks';
 import postTask from './routes/postTask';
 
+interface Task { 
+  id: number; 
+  title: string;
+}
+
+let tasks: Task[] = [];
+
 function router(req: IncomingMessage, res: ServerResponse): void {
   const { method, url } = req;
 
   if (method === 'GET' && url === '/tasks') {
-    return getTasks(req, res);
+    return getTasks(req, res, tasks);
   }
 
   if (method === 'POST' && url === '/tasks') {
-    return postTask(req, res);
+    return postTask(req, res, tasks);
   }
 
   res.writeHead(404, { 'Content-Type': 'application/json' });
